97234a0e619b91fd780f7f90a71652bc9b79d56e
Chaman <76839908+chamanbravo@users.noreply.github.com>
2021-03-22 17:05:43 +0545

    added map

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([27.677512, 85.331790], 15);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 20,
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
 }).addTo(mymap);
+
+
+const icon = L.icon({
+    iconUrl: './images/icon-location.png',
+    iconSize: [50, 55],
+    iconAnchor: [23, 56],
+    });
+
+const marker = L.marker([27.677512, 85.331790],{icon: icon}).addTo(mymap);
