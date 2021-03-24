442ebf22eb40b8a595f64a4209e360eabc542bd6
Chaman <76839908+chamanbravo@users.noreply.github.com>
2021-03-24 20:09:50 +0545

    .

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const outputlocation = document.querySelector(".location");
 const timezone = document.querySelector(".timezone");
 const isp = document.querySelector(".isp");
 const button = document.querySelector(".sumbit");
-const userInput = document.querySelector(".myInput").value;
+const userInput = document.querySelector(".myInput");
 
 
 //Ipify//
@@ -40,3 +40,41 @@ async function getMyIp(){
 
 
 getMyIp();
+
+
+const search = async () => {
+
+    const geoLocation = await fetch (`https://geo.ipify.org/api/v1?apiKey=at_bOCEyrwTfQSxAHyZ7CypYXPaSQQXC&ipAddress=${userInput.value}`);
+    const dataGeoLocation = await geoLocation.json();
+
+	outputIp.innerHTML = dataGeoLocation.ip;
+	outputlocation.innerHTML = `${dataGeoLocation.location.city}, ${dataGeoLocation.location.country}`;
+	timezone.innerHTML = dataGeoLocation.location.timezone;
+	isp.innerHTML = dataGeoLocation.isp;
+
+
+    var mymap = L.map('mapid').setView([dataGeoLocation.location.lat, dataGeoLocation.location.lng], 15);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+			maxZoom: 20,
+			id: 'mapbox/streets-v11',
+			tileSize: 512,
+			zoomOffset: -1
+	}).addTo(mymap);
+
+
+	const icon = L.icon({
+	    iconUrl: './images/icon-location.png',
+	    iconSize: [50, 55],
+	    iconAnchor: [23, 56],
+	    });
+
+	const marker = L.marker([dataGeoLocation.location.lat, dataGeoLocation.location.lng],{icon: icon}).addTo(mymap);
+
+};
+
+
+button.addEventListener('click', () => {
+	search();
+});
+
